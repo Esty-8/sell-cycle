2db3f4ae71cde946b040b34077a0d1e28562246b
Esty-8 Bechian <esterabechian12@outlook.com>
2024-09-24 21:27:47 +0000

    added categories to the admin

--- product/models.py
+++ product/models.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-class Category(models.Model):
+class Category(models.Model): # Define a Category model with fields for name
     name = models.CharField(max_length=200)
 
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model): # Define a Product model with fields for name, description, and category
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True) # in case the product doesn't have a description
+    price = models.FloatField() 
+    is_sold = models.BooleanField(default=False) # to track if the product has been sold
+    created_at = models.DateTimeField(auto_now_add=True) # automatically set the created_at field to the current date and time when the product is created
+    created_at = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE) # links the Product model to the Username model using a foreign key
