bdbbad5e3aad9c059ba69dc7ef045f240373d309
Esty-8 Bechian <esterabechian12@outlook.com>
2024-09-25 16:27:55 +0000

    added static folder and images default

--- sell/views.py
+++ sell/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from product.models import Category, Product  # Import the Product model
 
 # Create your views here.
 def index(request):
-    return render(request, 'sell/index.html')
+    products = Product.objects.filter(is_sold=False)[:6]
+    categories = Category.objects.all()  # Get all categories
+
+    # Correct the return statement by passing the context as the third argument to render()
+    return render(request, 'sell/index.html', {
+        'categories': categories,  # Pass the categories to the template for display.
+        'products': products,
+    })
 
 def contact(request):
     return render(request, 'sell/contact.html')
