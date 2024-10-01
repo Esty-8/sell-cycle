cd82c91f0fd37361c18fe12f99a1175a675e111c
Esty-8 Bechian <esterabechian12@outlook.com>
2024-10-01 18:03:30 +0000

    fixed image not displaying on the main page

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
 from .models import Category, Product  # Import the Category and Product models.
 
 admin.site.register(Category)  # Register the Category model in the admin interface.
 admin.site.register(Product)  # Register the Product model in the admin interface.
+
 
--- product/models.py
+++ product/models.py
@@ -23,9 +23,9 @@ class Category(models.Model): # Define a Category model with fields for name
 class Product(models.Model): # Define a Product model with fields for name, description, and category
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE) # links the Product model to the Category model using a foreign key
     name = models.CharField(max_length=200)
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('Feature Image', default='placeholder') # <--------
     description = models.TextField(blank=True, null=True) # in case the product doesn't have a description
-    image = CloudinaryField('image', blank=True, null=True)  # Use CloudinaryField
+    image = CloudinaryField('image', blank=True, null=True)  # Use CloudinaryField #
     price = models.FloatField() 
     is_sold = models.BooleanField(default=False) # to track if the product has been sold
     created_at = models.DateTimeField(auto_now_add=True) # automatically set the created_at field to the current date and time when the product is created
