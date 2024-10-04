da4930a4fe51bfd11fcb2f24f7907b505eab2c32
Esty-8 Bechian <esterabechian12@outlook.com>
2024-10-04 15:49:17 +0000

    authentication  fixed

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-esty8-sellcycle-eiy4e7a4ns9.ws-eu116.gitpod.io',
                  '.herokuapp.com'
