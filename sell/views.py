feb9a27e398923c71a708db7cdf64211a6cf4744
Estera Bechian <x4d0cil4@students.codeinstitute.net>
2024-10-10 12:49:02 +0000

    resetting migrations and reconnecting to the Database

--- sell/views.py
+++ sell/views.py
@@ -6,7 +6,7 @@ from .forms import SignupForm # Import the Signup
 
 # Create your views here.
 def index(request):
-    products = Product.objects.filter(is_sold=False)[:6]
+    products = Product.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()  # Get all categories
 
     # Correct the return statement by passing the context as the third argument to render()
