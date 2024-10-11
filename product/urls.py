8f046dc87f17ecd3109676fdfdd39290c9385903
Estera Bechian <x4d0cil4@students.codeinstitute.net>
2024-10-11 10:32:45 +0000

    Made sure the product_information view is correctly defined to handle requests for a single product

--- product/urls.py
+++ product/urls.py
@@ -10,6 +10,6 @@ app_name = 'product'  # line for namespacing
 
 urlpatterns = [
     path('new/', views.new_product, name='new_product'), # new_product is the name of the view function
-    path('<int:pk>/', views.information, name='product_information'), # product_information is the name of the view function
+    path('<int:pk>/', views.product_information, name='product_information'), # product_information is the name of the view function
 ]
 
--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required 
 
 
@@ -8,7 +8,7 @@ from .models import Product  # Import the Product model
 from .forms import NewProductForm # Import the NewProduct
 
 # Create your views here.
-def information(request, pk):
+def product_information(request, pk):
     # Retrieve the product information from the database using the provided primary key.
     product = get_object_or_404(Product, pk=pk)
 
@@ -27,7 +27,7 @@ def new_product(request):
             product = form.save(commit=False)  # Create a product instance but don't save it to the database yet
             product.created_by = request.user  # Set the created_by field to the logged-in user
             product.save()  # Now save the product instance to the database
-            return redirect('/')  # Redirect to the product list page after saving
+            return redirect('product:product_information', pk=product.id)  # Redirect to the product list page after saving
             
     else:
         form = NewProductForm()  # If GET request, display an empty form
